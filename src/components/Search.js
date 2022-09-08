5d9a8c0acf215e8f2f57fc1c2075cea590090878
rhk810 <110488142+rhk810@users.noreply.github.com>
2022-09-08 15:08:09 +0530

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -33,7 +33,7 @@ const Search = () => {
         clearTimeout(timeoutId);
       };
     }
-  }, [term, results.length]);
+  }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
